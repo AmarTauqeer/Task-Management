c34268a25fd337a2d319eb77366237380b68b9d6
unknown <amar.tauqeer@gmail.com>
2020-06-21 17:35:21 +0200

    Updates

---
+++ precache-manifest.efbdcb2c12e9435c3da5bfa020bb3b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83528d00c1e8e661fbe0fd76f329ae0a",
+    "url": "/Task-Management/index.html"
+  },
+  {
+    "revision": "abb9596b6876f9bbeb3a",
+    "url": "/Task-Management/static/css/main.5ab1657a.chunk.css"
+  },
+  {
+    "revision": "45cad6b63060c03096dc",
+    "url": "/Task-Management/static/js/2.00b34d08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Management/static/js/2.00b34d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb9596b6876f9bbeb3a",
+    "url": "/Task-Management/static/js/main.beaff0dc.chunk.js"
+  },
+  {
+    "revision": "6b7935f464380e74096c",
+    "url": "/Task-Management/static/js/runtime-main.3ef2b79a.js"
+  }
+]);
